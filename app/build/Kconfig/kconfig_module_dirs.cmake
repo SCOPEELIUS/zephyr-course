667858960acb8b88bd9fc1475d00bb6634fd6917
ELIUS-FAUSTINE <scopeelius@gmail.com>
2026-05-19 00:19:10 +0300

    Implement l2-task1: LED blink application

---
+++ app/build/Kconfig/kconfig_module_dirs.cmake
@@ -0,0 +1,5 @@
+set(kconfig_env_dirs)
+list(APPEND kconfig_env_dirs ZEPHYR_CMSIS_6_MODULE_DIR=D:/zephyr-course/deps/modules/hal/cmsis_6)
+list(APPEND kconfig_env_dirs ZEPHYR_HAL_ESPRESSIF_MODULE_DIR=D:/zephyr-course/deps/modules/hal/espressif)
+list(APPEND kconfig_env_dirs ZEPHYR_HAL_NORDIC_MODULE_DIR=D:/zephyr-course/deps/modules/hal/nordic)
+list(APPEND kconfig_env_dirs ZEPHYR_HAL_STM32_MODULE_DIR=D:/zephyr-course/deps/modules/hal/stm32)
