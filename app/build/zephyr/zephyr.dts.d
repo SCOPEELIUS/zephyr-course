1a4c38c4cf9f04b1ab640774a67f2fd0fdd5310a
ELIUS-FAUSTINE <scopeelius@gmail.com>
2026-05-19 09:37:37 +0300

    Implementing  l5-task1: custom board

--- app/build/zephyr/zephyr.dts.d
+++ app/build/zephyr/zephyr.dts.d
@@ -1,5 +1,5 @@
 empty_file.o: D:/zephyr-course/deps/zephyr/misc/empty_file.c \
- D:/zephyr-course/deps/zephyr/boards/espressif/esp32s3_devkitc/esp32s3_devkitc_procpu.dts \
+ D:/zephyr-course/app/boards/our_board/our_board_procpu.dts \
  D:/zephyr-course/deps/zephyr/dts/xtensa/espressif/esp32s3/esp32s3_wroom_n8.dtsi \
  D:/zephyr-course/deps/zephyr/dts/xtensa/espressif/esp32s3/esp32s3_common.dtsi \
  D:/zephyr-course/deps/zephyr/dts/common/mem.h \
@@ -25,7 +25,7 @@ empty_file.o: D:/zephyr-course/deps/zephyr/misc/empty_file.c \
  D:/zephyr-course/deps/zephyr/include/zephyr/dt-bindings/input/esp32-touch-sensor-input.h \
  D:/zephyr-course/deps/zephyr/dts/vendor/espressif/partitions_0x0_amp.dtsi \
  D:/zephyr-course/deps/zephyr/dts/vendor/espressif/partitions_0x0_amp_4M.dtsi \
- D:/zephyr-course/deps/zephyr/boards/espressif/esp32s3_devkitc/esp32s3_devkitc-pinctrl.dtsi \
+ D:/zephyr-course/app/boards/our_board/esp32s3_devkitc-pinctrl.dtsi \
  D:/zephyr-course/deps/zephyr/include/zephyr/dt-bindings/pinctrl/esp-pinctrl-common.h \
  D:/zephyr-course/deps/zephyr/include/zephyr/dt-bindings/pinctrl/esp32s3-gpio-sigmap.h \
  boards/esp32s3_devkitc.overlay \
